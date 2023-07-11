f7b8d95cdde26c8915348a42744ec591c71b1cc5
GeraAP <anna.gerasymenyuk@gmail.com>
2023-07-11 22:24:28 +0200

    a

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcons from "./WeatherIcons";
-import WeatherTemperature from "./WeatherTemperature";
+import WeatherTemperature from "./Temperature";
 
 
 
